3069d32e4914cc518272e1ae09b9873f34c0b111
jaeeon99 <dlwodjs13579@gmail.com>
2024-11-29 15:01:55 +0900

    update : Listveiw 메소드 활용

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -23,74 +23,81 @@ class HomePage extends StatelessWidget {
               height: 20,
             ),
             Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    width: double.infinity,
-                    height: 120,
-                    child: Stack(
-                      children: [
-                        //
-                        Positioned(
-                          right: 0,
-                          width: 120,
-                          height: 120,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20),
-                            child: Image.network(
-                              'https://picsum.photos/200/300',
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                        Container(
-                          width: double.infinity,
-                          height: double.infinity,
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(20)),
-                          margin: EdgeInsets.only(right: 100),
-                          padding: EdgeInsets.all(20),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Today I Learned',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 14,
-                                ),
-                              ),
-                              Spacer(),
-                              Text(
-                                'Flutter Gridview 를 배움',
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                  color: Colors.grey,
-                                  fontSize: 12,
-                                ),
-                              ),
-                              SizedBox(
-                                height: 4,
-                              ),
-                              Text(
-                                '2024.11.29.',
-                                style: TextStyle(
-                                  color: Colors.grey,
-                                  fontSize: 12,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+              child: ListView.separated(
+                itemCount: 10,
+                separatorBuilder: (context, index) => SizedBox(
+                  height: 10,
+                ),
+                itemBuilder: (context, index) {
+                  return item();
+                },
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Container item() {
+    return Container(
+      width: double.infinity,
+      height: 120,
+      child: Stack(
+        children: [
+          //
+          Positioned(
+            right: 0,
+            width: 120,
+            height: 120,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                'https://picsum.photos/200/300',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(20)),
+            margin: EdgeInsets.only(right: 100),
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Today I Learned',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  'Flutter Gridview 를 배움',
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 12,
+                  ),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  '2024.11.29.',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
