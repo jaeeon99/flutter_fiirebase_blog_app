1b072cd230af1ba10b644490a21eadb5b849a906
jaeeon99 <dlwodjs13579@gmail.com>
2024-11-29 14:17:33 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_fiirebase_blog_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_fiirebase_blog_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/ui/home/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(Object context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text('Homepage'),
+    );
+  }
+}
