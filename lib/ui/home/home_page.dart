de636cad392edbf6c1411d80015fce34654d8406
jaeeon99 <dlwodjs13579@gmail.com>
2024-11-29 15:07:08 +0900

    update : listview 선택 시 상세페이지 이동

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fiirebase_blog_app/ui/detail/detail_page.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -39,66 +40,81 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Container item() {
-    return Container(
-      width: double.infinity,
-      height: 120,
-      child: Stack(
-        children: [
-          //
-          Positioned(
-            right: 0,
-            width: 120,
-            height: 120,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.network(
-                'https://picsum.photos/200/300',
-                fit: BoxFit.cover,
+  Widget item() {
+    return Builder(builder: (context) {
+      return GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return DetailPage();
+              },
+            ),
+          );
+        },
+        child: Container(
+          width: double.infinity,
+          height: 120,
+          child: Stack(
+            children: [
+              //
+              Positioned(
+                right: 0,
+                width: 120,
+                height: 120,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                    'https://picsum.photos/200/300',
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-            ),
+              Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20)),
+                margin: EdgeInsets.only(right: 100),
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Today I Learned',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      'Flutter Gridview 를 배움',
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 12,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      '2024.11.29.',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(20)),
-            margin: EdgeInsets.only(right: 100),
-            padding: EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Today I Learned',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 14,
-                  ),
-                ),
-                Spacer(),
-                Text(
-                  'Flutter Gridview 를 배움',
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 12,
-                  ),
-                ),
-                SizedBox(
-                  height: 4,
-                ),
-                Text(
-                  '2024.11.29.',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 12,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+        ),
+      );
+    });
   }
 }
