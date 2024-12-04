c304bea55eeb40b9f1f63a8d05182b323eb1bd0f
jaeeon99 <dlwodjs13579@gmail.com>
2024-12-04 13:42:33 +0900

    add: detail_view_model

--- lib/ui/detail/detail_view_model.dart
+++ lib/ui/detail/detail_view_model.dart
@@ -3,14 +3,26 @@
 
 // 2. 뷰 모델 만들기
 import 'package:flutter_fiirebase_blog_app/data/model/post.dart';
+import 'package:flutter_fiirebase_blog_app/data/repository/post_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class DetailViewModel extends AutoDisposeFamilyNotifier<Post, Post> {
   @override
   Post build(Post arg) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return arg;
+  }
+
+  Future<bool> deletPost() async {
+    final postRepository = PostRepository();
+    return await postRepository.delete(arg.id);
   }
 }
 
 // 3. 뷰 모델 관리자 만들기
+
+final detailViewModelProvider =
+    NotifierProvider.autoDispose.family<DetailViewModel, Post, Post>(
+  () {
+    return DetailViewModel();
+  },
+);
